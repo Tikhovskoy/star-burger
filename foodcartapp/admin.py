4c744bf99fd150c8dfa42f32d7c929543fe6a00b
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-05-19 16:48:23 +0300

    редирект обратно в интерфейс менеджера после редактирования заказа в админке

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
 from django.utils.html import format_html
-
+from django.utils.http import url_has_allowed_host_and_scheme
 from .models import (
     Product, ProductCategory, Restaurant, RestaurantMenuItem,
     Order, OrderItem,
@@ -105,6 +105,16 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['firstname', 'lastname', 'phonenumber', 'address', 'created_at']
     search_fields = ['firstname', 'lastname', 'phonenumber', 'address']
     inlines = [OrderItemInline]
+
+    def response_change(self, request, obj):
+        next_url = request.GET.get('next')
+        if next_url and url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure(),
+        ):
+            return redirect(next_url)
+        return super().response_change(request, obj)
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
