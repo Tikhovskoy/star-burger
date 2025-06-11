1fe02d78fa272e1fa0400ee50c5d39604fbf763f
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-06-11 04:34:05 +0300

    setup rollbar

--- foodcartapp/urls.py
+++ foodcartapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from .views import OrderView, banners_list_api, product_list_api
-from .views import trigger_error 
 
 app_name = "foodcartapp"
 
@@ -9,5 +8,4 @@ urlpatterns = [
     path("products/", product_list_api),
     path("banners/", banners_list_api),
     path("order/", OrderView.as_view()),
-    path('test-rollbar/', trigger_error),
 ]
