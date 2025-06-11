bb9796e673a436b107c135f855e3134be5d90614
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-06-11 12:59:20 +0300

    PostgreSQL DB.

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -89,7 +89,8 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=60,
     )
 }
 
